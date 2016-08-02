acd7c8f7a4c70518a52474b78ecd28ca3404d696
jinhan <569929309@qq.com>
2016-08-02 12:14:05 +0800

    'good'

--- source/taocomment.py
+++ source/taocomment.py
@@ -15,6 +15,46 @@ import xml.dom.minidom
 import json
 from openpyxl import Workbook
 from openpyxl import load_workbook
+import datetime
+
+'''
+
+时间过期函数
+输入: 
+	year 年
+	month 月
+	day 日
+输出:
+	第一个返回参数:True 表示时间过期,False 表示时间没过期
+	第二个返回参数:表示剩余过期天数
+
+example:
+
+	if __name__ == '__main__':
+		# 明天的时间
+		year = 2016
+		month = 7
+		day =19
+		ispass,remainday = ispasstime(year,month,day)
+		if ispass:
+			print("还没有过期,剩余天数:"+str(remainday))
+		else:
+			print("已经过期,过期天数"+str(remainday))
+
+'''
+def ispasstime(year,month,day):
+	# 今天的时间
+	nowtoday = datetime.datetime.today()
+	# 过期时间
+	passday = datetime.datetime(year,month,day)
+	# 剩余天数
+	remaindays = (nowtoday-passday).days
+	# 判断
+	if remaindays <= 0:
+		return True,-remaindays
+	else:
+		return False,remaindays
+
 
 def password():
 	print('请输入你的账号和密码')
@@ -286,7 +326,7 @@ def tmall(returncomment,max=99):
 		# print(tjson)
 
 		# JSON解码
-		print(tjson)
+		#print(tjson)
 		tjson=json.loads(tjson)
 
 
@@ -327,6 +367,16 @@ def tmall(returncomment,max=99):
 	return returncomment
 
 if __name__ == '__main__':
+	# 明天的时间
+	year = 2016
+	month = 12
+	day =18
+	ispass,remainday = ispasstime(year,month,day)
+	if ispass:
+		print("还没有过期,剩余天数:"+str(remainday))
+	else:
+		print("已经过期,过期天数"+str(remainday))
+		exit(1)
 	# 欢迎语
 	begin()
 
